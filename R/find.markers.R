64d01e09103aed1036861090e56cf5a74f92a35e
dleelab <dustorm@gmail.com>
2017-03-16 19:26:19 -0400

    new function find.markers()

---
+++ R/find.markers.R
@@ -0,0 +1,41 @@
+#' A function for finding markers for hidden factors
+#'
+#' find.markers() returns a read counts matrix of markers for each hidden factor detected by IA-SVA.
+#'  
+#'
+#' @param Y The read counts matrix with samples in row and genes in column.
+#' @param iasva.sv  The matrix of estimated surrogate variables, one column for each surrogate variable. 
+#' @param method The multiple testing adjustment method, default="BH".
+#' @param sig.cutoff The cutoff for the significance.
+#' @param rsq.cutoff The cutoff for the R squared.
+#' @param verbose If verbose=TRUE, the function outputs detailed messages. 
+#'
+#' @return marker.counts The matrix of read counts for markers, one column for each cell. 
+#' 
+#' @examples
+#' 
+#' @export
+#'
+
+find.markers <- function(Y, iasva.sv, method="BH", sig.cutoff=0.05, rsq.cutoff=0.3, verbose=FALSE){
+  if(min(Y)<0){ Y <- Y + abs(min(Y)) }
+  lY <- log(Y+1)
+  all.markers <- NULL
+  num.sv <- ncol(iasva.sv)
+  for(i in 1:num.sv){
+    fit <- lm(lY~iasva.sv[,i])
+    pval.vec <- unlist(lapply(summary(fit), function(x) x$coefficient[2,4]))
+    rsq.vec <- unlist(lapply(summary(fit), function(x) x$adj.r.squared))
+    adj.pval.vec <- p.adjust(pval.vec, method=method, n= length(pval.vec))
+    markers <- rownames(counts)[adj.pval.vec < sig.cutoff & rsq.vec > rsq.cutoff]
+    cat(paste0("# of markers (",colnames(iasva.sv)[i],"): ", length(markers),"\n"))
+    all.markers <- c(all.markers, markers)
+  }
+  cat("total # of unique markers: ",length(all.markers))
+  all.markers <- unique(all.markers)
+  length(all.markers)
+  marker.counts <- counts[rownames(counts)%in%all.markers,]
+  dim(marker.counts)
+  return(marker.counts)
+}
+
