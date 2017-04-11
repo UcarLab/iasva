9e58715d805205495c6678354b88a4a4599648ea
dleelab <dustorm@gmail.com>
2017-04-11 16:53:13 -0400

    updated.

---
+++ inst/doc/tSNE_post_IA-SVA_7celltypes_new.R
@@ -0,0 +1,105 @@
+## ----load_packages, echo=TRUE--------------------------------------------
+rm(list=ls())
+library(iasva)
+library(iasvaExamples)
+library(Rtsne)
+library(pheatmap)
+library(corrplot)
+
+## ----load_data, echo=TRUE------------------------------------------------
+data("Lawlor_Islet_scRNAseq_Read_Counts")
+data("Lawlor_Islet_scRNAseq_Annotations")
+ls()
+counts <- Lawlor_Islet_scRNAseq_Read_Counts
+anns <- Lawlor_Islet_scRNAseq_Annotations
+dim(anns)
+dim(counts)
+# set seed for the random number generator.
+set.seed(344532456)
+
+## ----alpha_cells, echo=TRUE, results='asis'------------------------------
+counts <- counts[, (anns$Cell_Type!="none")] 
+anns <- subset(anns, (Cell_Type!="none"))
+dim(counts)
+dim(anns)
+
+anns <- droplevels(anns)
+
+prop.zeros <- sum(counts==0)/length(counts)
+prop.zeros
+
+filter = apply(counts, 1, function(x) length(x[x>5])>=3)
+
+counts = counts[filter,]
+dim(counts)
+
+prop.zeros <- sum(counts==0)/length(counts)
+prop.zeros
+
+Patient_ID <- anns$Patient_ID
+Cell_Type <- anns$Cell_Type
+Phenotype <- anns$Phenotype
+Batch <- anns$Batch
+
+## ----geno_lib_size, echo=TRUE, fig.width=6, fig.height=4-----------------
+Geo_Lib_Size <- colSums(log(counts+1))
+barplot(Geo_Lib_Size, xlab="Cell")
+lcounts <- log(counts + 1)
+pca.res = svd(lcounts - rowMeans(lcounts))$v
+cor(Geo_Lib_Size, pca.res[,1])
+
+## ----run_tsne, echo=TRUE, fig.width=6, fig.height=6----------------------
+tsne.res <- Rtsne(t(lcounts), dims = 2)
+
+plot(tsne.res$Y, main="tSNE", pch=21, bg=c("red","green3","blue","grey","orange","black","purple")[Cell_Type], oma=c(4,4,6,12))
+legend(-20, 25, levels(Cell_Type), fill=c("red", "green3", "blue","grey","orange","black","purple"), bty="n")
+
+## ----run_iasva, echo=TRUE, fig.width= 12, fig.height=10------------------
+mod <- model.matrix(~Phenotype+Patient_ID+Batch+Geo_Lib_Size)
+iasva.res<- iasva(t(counts), mod[,-1],verbose=FALSE, permute=FALSE, num.sv=10)
+iasva.sv <- iasva.res$sv
+
+pairs(iasva.sv, main="IA-SVA", pch=21, bg=c("red","green3","blue","grey","orange","black","purple")[Cell_Type], oma=c(4,4,6,12))
+legend(0.82, 0.65, levels(Cell_Type), fill=c("red", "green3", "blue","grey","orange","black","purple"), bty="n")
+
+corrplot(cor(cbind(Geo_Lib_Size,iasva.sv)))
+
+## ----find_markers_SV10, echo=TRUE, fig.width=20, fig.height=5------------
+marker.counts <- find.markers(t(counts), as.matrix(iasva.sv[,c(10)]), sig.cutoff = 0.01, rsq.cutoff = 0.6)
+nrow(marker.counts)
+
+anno.col <- data.frame(Cell_Type=Cell_Type)
+rownames(anno.col) <- colnames(marker.counts)
+head(anno.col)
+pheatmap(log(marker.counts+1), show_colnames =FALSE, show_rownames = TRUE, clustering_method = "ward.D2",cutree_cols = 2,annotation_col = anno.col)
+
+## ----find_markers2, echo=TRUE, fig.width=6, fig.height=5-----------------
+marker.counts <- find.markers(t(counts), as.matrix(iasva.sv[,c(1,3,4,6)]))
+nrow(marker.counts)
+
+anno.col <- data.frame(Cell_Type=Cell_Type)
+rownames(anno.col) <- colnames(marker.counts)
+head(anno.col)
+pheatmap(log(marker.counts+1), show_colnames =FALSE, show_rownames = FALSE, clustering_method = "ward.D2",cutree_cols = 7,annotation_col = anno.col)
+
+## ----run_tsne_post_iasva, echo=TRUE, fig.width=6, fig.height=6-----------
+set.seed(75458456)
+tsne.res <- Rtsne(unique(t(log(marker.counts+1))), dims = 2)
+plot(tsne.res$Y, main="tSNE", pch=21, bg=c("red","green3","blue","grey","orange","black","purple")[Cell_Type], oma=c(4,4,6,12))
+legend(12, 20, levels(Cell_Type), fill=c("red", "green3", "blue","grey","orange","black","purple"), bty="n")
+
+
+
+## ----run_tsne_post_iasva2, echo=TRUE, fig.width=12, fig.height=10--------
+set.seed(3455563)
+tsne.res <- Rtsne(pca.res[,c(1:9)], dims = 2)
+plot(tsne.res$Y, main="tSNE", pch=21, bg=c("red","green3","blue","grey","orange","black","purple")[Cell_Type], oma=c(4,4,6,12))
+legend(12, 20, levels(Cell_Type), fill=c("red", "green3", "blue","grey","orange","black","purple"), bty="n")
+
+pairs(pca.res[,1:10], main="PCA", pch=21, bg=c("red","green3","blue","grey","orange","black","purple")[Cell_Type], oma=c(4,4,6,12))
+legend(0.82, 0.65, levels(Cell_Type), fill=c("red", "green3", "blue","grey","orange","black","purple"), bty="n")
+
+
+## ----session_info, echo=TRUE---------------------------------------------
+sessionInfo()
+
